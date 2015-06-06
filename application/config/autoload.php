6c4aae3254a7f649ae1d2178caffda37aa8684be
pujie <pw.prayitno@gmail.com>
2015-06-06 21:33:41 +0700

    add configs

---
+++ application/config/autoload.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	$autoload['packages'] = array(APPPATH.'third_party');
+	$autoload['libraries'] = array(
+		'database',
+		'datamapper',
+		'pagination',
+		'session',
+		'common',
+		'authentication',
+		'email',
+		'template',
+		'form_validation',
+		'Padiauth',
+	);
+	$autoload['helper'] = array('url','form','date','file','directory','text','cookie');
+	$autoload['config'] = array();
+	$autoload['language'] = array();
+	$autoload['model'] = array(array(
+		'App_logs/app_log',
+		'App_settings/app_setting',
+		'Blogs/blog',
+		'Categories/category',
+		'Galleries/gallery',
+		'Groups/group',
+		'Kelas/kelas',
+		'Users/user',
+		'Websettings/websetting'
+	));
