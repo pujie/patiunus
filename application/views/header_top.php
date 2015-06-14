80e4a79deab9704fcf31d9b5861ddaccd7620a88
pujie <pw.prayitno@gmail.com>
2015-06-14 20:53:12 +0700

    add header_top

---
+++ application/views/header_top.php
@@ -0,0 +1,8 @@
+<div class="header_top">
+	<h1><?php echo $websetting->webtitle;?></h1>
+	<div class="header_top_right">
+	<div class="search_box">
+		<div class="clear"></div>
+	</div>
+</div>
+<div class="clear"></div>
